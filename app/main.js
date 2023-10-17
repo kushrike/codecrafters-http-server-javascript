7120d1da1cf64493046be8005feadf994ebec26f
Kushal Srivastava <kushal.srivastava@captainfresh.in>
2023-10-17 15:27:56 +0530

    3#1

--- app/main.js
+++ app/main.js
@@ -6,13 +6,22 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        const request = data.toString().split("\r\n");
+        const requestLine = request[0].split(" ");
+        const path = requestLine[1];
+
+        if (path === "/") {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
         socket.end();
     });
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
+
+    socket.on("close", () => {
+      socket.end();
+      server.close();
+    });
 });
 
 server.listen(4221, "localhost");
