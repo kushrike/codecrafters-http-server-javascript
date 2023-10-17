790d6b9ed2adf61ac0443ead1a9d0388bff468b2
Kushal Srivastava <kushal.srivastava@captainfresh.in>
2023-10-17 15:19:24 +0530

    2#1

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
     socket.on("data", () => {
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    })
+    });
   socket.on("close", () => {
     socket.end();
     server.close();
