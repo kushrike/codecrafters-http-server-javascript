c1d582bad433307b0319ce5eed95e03a40da0c3e
Kushal Srivastava <kushal.srivastava@captainfresh.in>
2023-10-20 18:25:38 +0530

    7#3

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@ var path =  require("path");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-const args = process.argv.slice(2);
+const args = process.argv;
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
