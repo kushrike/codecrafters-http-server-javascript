dce2791583da510960e259996728953ed38038f0
Kushal Srivastava <kushal.srivastava@captainfresh.in>
2023-10-20 18:28:31 +0530

    7 dbg

--- app/main.js
+++ app/main.js
@@ -38,7 +38,7 @@ const server = net.createServer((socket) => {
         else if(path.startsWith("/files/")) {
           const filename = path.substring(7);
           try {
-            console.log(args);
+            console.log("filename", filename);
             const file = fs.readFileSync(path.join(args[1], filename));
             console.log(path.join(args[1], filename));
             socket.write("HTTP/1.1 200 OK\r\n");
